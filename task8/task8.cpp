b3969513da583b1a989de55bd2384c86a7a232ed
konovashka <anton_konovalov_1@mail.ru>
2021-11-12 18:52:32 +0300

    Добавьте файлы проекта.

---
+++ task8/task8.cpp
@@ -0,0 +1,29 @@
+﻿//проверка на простоту 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n,y,count;
+	y = 1;
+	count = 0;
+	scanf_s("%d", &n);
+	while (y<=n/2)
+	{
+		if (n%y==0)
+		{
+			count += 1;
+		}
+		y += 1;
+		printf("%d %d\n", y,count);
+	}
+	if (count==1)
+	{
+		printf("prostoe");
+	}
+	else
+	{
+		printf("ne prostoye");
+	}
+}
